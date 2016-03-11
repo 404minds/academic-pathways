66cf2107454b06e4f50f7cc33982a146bdadac18
Rinki Agarwal <niftyrinki@gmail.com>
2016-03-11 16:08:12 +0530

    fix the left and right slider

--- js/main.js
+++ js/main.js
@@ -5,6 +5,31 @@ $('a').click(function() {
     return false;
 });
 
-
+var currentSlide = 1;
 var left = document.getElementById('left');
 var right = document.getElementById('right');
+var wrapper = document.getElementsByClassName('text-wrapper');
+var width = document.getElementById('slide').getBoundingClientRect().width;
+
+for(var i = 1, l = wrapper.length; i < l; i++) {
+  wrapper[i].style.left = width * i + "px";
+  wrapper[i].style.opacity = 1;
+}
+
+left.addEventListener("click",function() {
+	if(currentSlide == 1)
+		return;
+	document.getElementById('slide' + currentSlide).style.left = width + "px";
+  document.getElementById('slide' + (--currentSlide)).style.left = '0';
+},false);
+
+right.addEventListener("click",function() {
+	if(currentSlide == 3)
+		return;
+	document.getElementById('slide' + currentSlide).style.left = -1 * width + "px";
+  document.getElementById('slide' + (++currentSlide)).style.left = '0';
+},false);
+
+
+
+
