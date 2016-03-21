da5220090d9a56dfed538d9042fd199f9d02e304
Rinki Agarwal <niftyrinki@gmail.com>
2016-03-21 13:13:28 +0530

    set SMTP configuration

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
 
-//set route 
-app.post('/',function(req,res){
-	res.send("hello"+req.body.name+"\nEmail:"+req.body.email+"\nContact:"+req.body.contact+"\nIntrested Course:"+req.body.course+"\nQualification:"+req.body.qualification+"\nExperince:"+req.body.experience+"\nLocation:"+req.body.location+"\n");
-})
+// set your routes 
+
+var route = require('./route')(app);
+
 
 // set port
 var port = process.env.PORT || 5000;
