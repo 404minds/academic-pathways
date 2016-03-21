c99856989f05894da7d9c735c9237c290b314209
Tushar Agarwal <niftytushar@gmail.com>
2016-03-21 13:02:38 +0530

    refactor app init

--- app.js
+++ app.js
@@ -6,15 +6,13 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
-
 //set route 
 app.post('/',function(req,res){
 	res.send("hello"+req.body.name+"\nEmail:"+req.body.email+"\nContact:"+req.body.contact+"\nIntrested Course:"+req.body.course+"\nQualification:"+req.body.qualification+"\nExperince:"+req.body.experience+"\nLocation:"+req.body.location+"\n");
 })
 
-// set port
-var port = process.env.PORT || 5000;
-
-app.listen(port);
-
-console.log("starting with port: " + port);
+// Initialize app
+var server = app.listen(process.env.PORT || 5000, function() {
+	var port = server.address().port;
+    console.log("Academic Pathways App is now running on port", port);
+});
